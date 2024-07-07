40f24699e46a400d721e705c84018d62691a0ce5
lenncode <torresgerttred45@gmail.com>
2024-07-07 16:05:13 -0500

    Dependencia Jakarta Validation

--- src/main/java/com/centroinformacion/ProyectoDawiApplication.java
+++ src/main/java/com/centroinformacion/ProyectoDawiApplication.java
@@ -8,7 +8,7 @@ public class ProyectoDawiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProyectoDawiApplication.class, args);
-		System.out.println("SISTEMA CORRIENDO !");
+		System.out.println("Encantos WEB corriendo..!");
 	}
 
 }
